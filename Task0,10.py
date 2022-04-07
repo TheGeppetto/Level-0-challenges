0e88f14edf7e20bd65380d571d725cbf86e2e96c
Thokozani Mndebele <102554234+TheGeppetto@users.noreply.github.com>
2022-04-07 10:39:30 +0200

    Update Task0,10.py

--- Task0,10.py
+++ Task0,10.py
@@ -1,4 +1,5 @@
 def common_characters(input_string1, input_string2):
+    #I use the characters in the first string to check if they are present in the string.If yes then i create a list
     common_characters_list = []
     for x in input_string1:
         if x in input_string2:
