aa965213d3e8f52e770d2a234134b6b7cc7140e3
Thokozani Mndebele <102554234+TheGeppetto@users.noreply.github.com>
2022-04-07 14:25:37 +0200

    Update and rename Task0,5.py to task0_5.py

--- task0_5.py
+++ task0_5.py
@@ -1,5 +1,4 @@
 def area_of_triangle(length_A, length_B, length_C):
-  #I'll be calculating the area using the semiperimeter method.
     semi_perimeter = 1/2 * (length_A + length_B + length_C)
     area_squared = (semi_perimeter * (semi_perimeter - length_A) * (semi_perimeter - length_B) * (semi_perimeter-length_C))
     return area_squared ** (1/2)
