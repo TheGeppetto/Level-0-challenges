7962d8fefd9bca55ec7f93163084843de7c22ec5
Thokozani Mndebele <102554234+TheGeppetto@users.noreply.github.com>
2022-04-07 14:37:08 +0200

    Update and rename Task0,10.py to task0_10.py

--- task0_10.py
+++ task0_10.py
@@ -1,9 +1,12 @@
 def common_characters(input_string1, input_string2):
-    #I use the characters in the first string to check if they are present in the string.If yes then i create a list
+    input_string1 = input_string1.lower()
+    input_string2 = input_string2.lower()
     common_characters_list = []
+    
     for x in input_string1:
         if x in input_string2:
             common_characters_list.append(x)
+            
     return "Common Letters: " + ", ".join(str(i) for i in common_characters_list)
 
-print(common_characters("house", "computers"))
+print(common_characters("House", "Homputers"))
