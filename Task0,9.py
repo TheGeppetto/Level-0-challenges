93c7706ceb041fb2c0f0b6497f74de44a29aca0d
Thokozani Mndebele <102554234+TheGeppetto@users.noreply.github.com>
2022-04-07 10:37:56 +0200

    Update Task0,9.py

--- Task0,9.py
+++ Task0,9.py
@@ -1,10 +1,11 @@
 def get_vowel(input_string):
+    #I convert all the characters into lowercase and find the vowels and avoid repititions 
     input_string = input_string.lower()
     vowel_list = []
     for i in input_string:
         if i in ["a","e","i","o","u"]:
             if i not in vowel_list:
                 vowel_list.append(i)
-    return vowel_list
+    return "Vowels: " + ", ".join(str(i) for i in vowel_list)
 
 print((get_vowel("Umuzi")))
