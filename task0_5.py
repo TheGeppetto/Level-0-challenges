2c5c5f46fbe1e9b02fab89e1eb16e9bdfe96d5bd
Thokozani Mndebele <102554234+TheGeppetto@users.noreply.github.com>
2022-04-14 08:38:37 +0200

    Update task0_5.py

--- task0_5.py
+++ task0_5.py
@@ -1,6 +1,6 @@
-def area_of_triangle(length_A, length_B, length_C):
-    semi_perimeter = 1/2 * (length_A + length_B + length_C)
-    area_squared = (semi_perimeter * (semi_perimeter - length_A) * (semi_perimeter - length_B) * (semi_perimeter-length_C))
+def area_of_triangle(length_a, length_b, length_c):
+    semi_perimeter = 1/2 * (length_a + length_b + length_c)
+    area_squared = (semi_perimeter * (semi_perimeter - length_a) * (semi_perimeter - length_b) * (semi_perimeter-length_c))
     return area_squared ** (1/2)
 
 print(area_of_triangle(3, 4, 5))
