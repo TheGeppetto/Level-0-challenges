1fd41bed357276e7e16e9c3bf3c54ac64d1f781a
Thokozani Mndebele <102554234+TheGeppetto@users.noreply.github.com>
2022-04-14 08:48:15 +0200

    Update task0_6.py

--- task0_6.py
+++ task0_6.py
@@ -1,10 +1,10 @@
 def maximum(*args):
-    max_number_holder = [""]
+    max_number_holder = None
     for x in args:
-        if max_number_holder[0] == "":
-            max_number_holder[0] = x
-        elif x > max_number_holder[0]:
-            max_number_holder[0] = x
-    return max_number_holder[0]
+        if max_number_holder == None:
+            max_number_holder = x
+        elif x > max_number_holder:
+            max_number_holder = x
+    return max_number_holder
 
-print(maximum(-2,-2,-5))
+print(maximum(-2,1,-5))
